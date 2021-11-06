7038000f6a14d80078a38af0db3079d3f5e36168
Oliver Oliverio <oliveroliverio@gmail.com>
2021-11-06 15:19:17 -0700

    feat: imported render_template

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def helloWorld():
-  return 'hello world'
+  return render_template('index.html')
 
 @app.route('/snapshot')
 def snapshot():
